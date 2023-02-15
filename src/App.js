d9e2a658922b8a8ebc7307be76354db11ecea55f
wallace-y <cameronwfinney@gmail.com>
2023-02-15 13:40:57 +0000

    adding default stying for css&search icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
+import SearchIcon from "./search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com/?apikey=290419c1"
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response  = await fetch(`${API_URL}&s=${title}`);
+        const data  = await response.json();
+
+        console.log(data.Search)
+    }
+
+    useEffect(() => {
+        searchMovies("Spiderman")
+
+    }, [])
+
+
+
     return (
         <h1>App</h1>
     );
